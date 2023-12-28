e21fcebefbd1d8171a148e0c33ba8f1f60a01e12
Kgothatso Theko <ST10092141@vcconnect.edu.za>
2023-12-28 23:19:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//Adding the questions
+const questions = [
+    {
+        question: 'Which is the largest animal in the world?',
+        answers: [
+            { text: 'Shark', correct: false},
+            { text: 'Shark', correct: false},
+            { text: 'Shark', correct: false},
+            { text: 'Shark', correct: false},
+        ] 
+    }
+]
