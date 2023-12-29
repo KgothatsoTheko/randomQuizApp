6cc34734dd0e2fb2a622f6a4e866eec0b56545c8
KgothatsoTheko <kgothatsotheko7@gmail.com>
2023-12-29 21:20:40 +0200

    javaScript commit

--- index.js
+++ index.js
@@ -4,9 +4,45 @@ const questions = [
         question: 'Which is the largest animal in the world?',
         answers: [
             { text: 'Shark', correct: false},
-            { text: 'Shark', correct: false},
-            { text: 'Shark', correct: false},
-            { text: 'Shark', correct: false},
+            { text: 'Blue Whale', correct: true},
+            { text: 'Elephant', correct: false},
+            { text: 'Giraffe', correct: false},
         ] 
-    }
+    }, 
+    {
+        question: 'Who is wealthiest?',
+        answers: [
+            { text: 'Elon Musk', correct: true},
+            { text: 'Bill Gates', correct: false},
+            { text: 'Mark Zuckerberg', correct: false},
+            { text: 'Jeff Bezos', correct: false},
+        ] 
+    },
+    {
+        question: 'Most influential rapper?',
+        answers: [
+            { text: 'J cole', correct: false},
+            { text: 'Kendrick Lamar', correct: true},
+            { text: 'Drake', correct: false},
+            { text: 'Eminem', correct: false},
+        ] 
+    },
+    {
+        question: 'Most popular platform?',
+        answers: [
+            { text: 'TikTok', correct: false},
+            { text: 'WhatsApp', correct: false},
+            { text: 'Instagram', correct: false},
+            { text: 'Facebook', correct: true},
+        ] 
+    },
+    {
+        question: 'Which power would you have?',
+        answers: [
+            { text: 'Flight', correct: false},
+            { text: 'Telekinesis', correct: true},
+            { text: 'Invisibility', correct: false},
+            { text: 'Telepathy', correct: true},
+        ] 
+    },
 ]
